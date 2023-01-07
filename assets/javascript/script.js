2ab8d4f4d91425899a88a73fc35ba264d5557e58
Guilherme Magno Matos <115946540+guilhermedevmatos15@users.noreply.github.com>
2023-01-07 17:28:57 -0300

    iniciando js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,14 @@
-// ! Em andamento...
+const input = document.querySelector('input');
+const groupOfFruits = [...document.querySelector('article.fruits').children];
+
+input.addEventListener('keyup', ()=>{
+   for (fruit of groupOfFruits) {
+      fruit.classList.add('hidden');
+   }
+   
+   for (let fruit of groupOfFruits) {
+      if (input.value.toLowerCase().includes(fruit.innerHTML.toLowerCase())) {
+         fruit.classList.remove('hidden');
+      }
+   }  
+});
